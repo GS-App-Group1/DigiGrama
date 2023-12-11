ef1a16a2fc42350876007197329474e98a6ec991
Themira Chathumina <110608712+ThemiraChathumina@users.noreply.github.com>
2023-12-11 09:34:44 +0530

    test

--- src/data/data.ts
+++
@@ -1,8 +0,0 @@
-// Define an interface for the object structure
-export default interface MyRequest {
-  name: string;
-  id: number;
-  date: string;
-  requestNumber: number;
-  status: string;
-}
