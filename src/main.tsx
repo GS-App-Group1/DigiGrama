02ca32660ed77a0f224e457b88b5ca4ebdcddfb7
Themira Chathumina <110608712+ThemiraChathumina@users.noreply.github.com>
2023-12-11 10:29:48 +0530

    asgardio

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { AuthProvider } from "@asgardeo/auth-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const config = {
+  signInRedirectURL: "https://localhost:3000/sign-in",
+  signOutRedirectURL: "https://localhost:3000/dashboard",
+  clientID: "c1PygEGq7_5SIKf5t_6fzmZ1u3Aa",
+  baseUrl: "https://api.asgardeo.io/t/interntest",
+  scope: ["openid", "profile"],
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthProvider config={config}>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
